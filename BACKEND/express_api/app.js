8f6ff6cd527fe1345d5b838adf7d5ea12fa75561
Rovic <sangabrielrovic@gmail.com>
2025-11-15 23:15:41 +0800

    Adjusted API BACKEND to support CORS

--- BACKEND/express_api/app.js
+++ BACKEND/express_api/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // <- add this line
 require('dotenv').config();
 
 app.use(express.json()); // parse JSON body
 app.use(express.urlencoded({ extended: true })); // optional, for form data
 
+app.use(cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'x-api-key'] // include your API key header
+}));
+
 const apiKeyAuth = require('./middleware/apiKeyAuth');
 const usersRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
-
-
 
 app.use(apiKeyAuth); // <- all routes below this line are protected
 app.use('/api/auth', authRouter);
--- BACKEND/express_api/middleware/apiKeyAuth.js
+++ BACKEND/express_api/middleware/apiKeyAuth.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
-  const apiKey = req.header('x-api-key'); // client sends header: x-api-key
-  if (!apiKey || apiKey !== process.env.API_KEY) {
+  const apiKey = req.header(process.env.API_KEY); // client sends header: x-api-key
+  if (!apiKey || apiKey !== process.env.API_VALUE) {
     return res.status(401).json({ message: 'Unauthorized: Invalid API Key' });
   }
   next(); // key is valid, continue
